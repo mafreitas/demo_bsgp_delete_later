15051bcfc612a4e46491d32228f323a92a23c831
Michael Freitas <osu8805@ascend-rw02.ten.osc.edu>
2026-06-30 12:12:13 -0400

    first commit and complete manual portion of Assignment 1

---
+++ manual/hello_Py.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Mike's Awesome Hello World Python Notebook
+
+# ## This is my first jupyter notebook
+
+# I am just showing off for the *class*
+
+
+print("Hello World")
+
+
